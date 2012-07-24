4943a8c87b1984dcccd6afdb4b3d5f0e43918609
tuelay <tuelay@192.168.2.103>
2012-07-24 22:29:52 +0200

    actions variable

--- gridviewform/CGridViewForm.php
+++ gridviewform/CGridViewForm.php
@@ -10,35 +10,44 @@
 Yii::import("zii.widgets.grid.CGridView");
 
 class CGridViewForm extends CGridView {
-    
+    public $actions=array();
     /**
      * Initialize the widget
      */
     public function init() {
+        if(!(is_array($this->columns[0]) and $this->columns[0]['class'] == 'CCheckBoxColumn')){
+            array_unshift($this->columns, array(
+                'class' => 'CCheckBoxColumn',
+                'id' => 'id', ));
+        }
+        //print_r($this->columns);
         parent::init();
         $this->selectableRows = 2;
         $this->template = "{items}\n{summary}{pager}";
         $this->summaryCssClass = "summary left";
+        if(count($this->actions)== 0){
+             $this->actions = array(' ' => ' ', 'create' => Yii::t('application', 'Erstellen'), 'delete' => Yii::t('application', 'Löschen'),);
+
+        }
     }
 
     /**
      * Run the widget
      */
     public function run() {
-        $actions = array(' ' => ' ', 'create' => Yii::t('application', 'Erstellen'), 'delete' => Yii::t('application', 'Löschen'),);
         $form = $this->beginWidget('CActiveForm', array(
             'id' => 'admin-form',
             'enableAjaxValidation' => false,
                 ));
         echo "<span class='right bottom ' >
-            <span style='margin-right: 15px;'>per Page ";
+            <span style='margin-right: 15px;'>".Yii::t('application', 'pro Seite').": ";
         $pageSize=Yii::app()->user->getState('pageSize',Yii::app()->params['defaultPageSize']);
         echo CHtml::dropDownList('pageSize',$pageSize,array(10=>10,20=>20,50=>50,100=>100),array(
                                   'onchange'=>"$.fn.yiiGridView.update('".$this->id."',{ data:{pageSize: $(this).val() }})",
                     )); 
         echo "</span>
-                <span>Action</span> ";
-        echo CHtml::dropDownList('action', '', $actions);
+                <span>".Yii::t('application', 'Action')."</span> ";
+        echo CHtml::dropDownList('action', '', $this->actions);
         $this->widget('zii.widgets.jui.CJuiButton', array(
             'name' => 'actionExecute',
             'buttonType' => 'button',
